9606d551c9ba52a0d442c1a7771c71809075fb9e
Omar Tarek <omartarek711@gmail.com>
2021-06-08 17:20:05 +0200

    initial commit (vue template with firebase)

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase/app'
+
+Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB0Pe6xU7axubg3y0lijR1r0fBH1Kn5k5U",
+  authDomain: "slack-with-vuejs.firebaseapp.com",
+  projectId: "slack-with-vuejs",
+  storageBucket: "slack-with-vuejs.appspot.com",
+  messagingSenderId: "1062602155241",
+  appId: "1:1062602155241:web:fe035db9ae9a4d3f193c78"
+};
+firebase.initializeApp(firebaseConfig);
+
+window.firebase = firebase;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="jumbotron bg-primary text-white text-center">
+            <h2 class="lead display-4">#Slack with VueJs#</h2>
+            <p>Realtime communication at it's best</p>
+        </div>
+        <div class="container-fluid">
+          <div class="row mt-5">
+            <div class="col text-center">
+              <button class="btn btn-outline-danger btn-lg mx-2">Login with Google</button>
+              <button class="btn btn-outline-info btn-lg mx-2">Login with Twitter</button>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "login",
+};
+</script>
