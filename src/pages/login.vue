6931ca30a419a6163901939ab10346e9e478905f
Omar Tarek <omartarek711@gmail.com>
2021-06-08 18:26:47 +0200

    Added Login with google BTN

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -5,18 +5,35 @@
             <p>Realtime communication at it's best</p>
         </div>
         <div class="container-fluid">
-          <div class="row mt-5">
-            <div class="col text-center">
-              <button class="btn btn-outline-danger btn-lg mx-2">Login with Google</button>
-              <button class="btn btn-outline-info btn-lg mx-2">Login with Twitter</button>
+            <div class="row mt-5">
+                <div class="col text-center">
+                    <button
+                        class="btn btn-outline-danger btn-lg mx-2"
+                        @click="loginwithGoogle"
+                    >
+                        Login with Google
+                    </button>
+                    <button class="btn btn-outline-info btn-lg mx-2">
+                        Login with Twitter
+                    </button>
+                </div>
             </div>
-          </div>
         </div>
     </div>
 </template>
 
 <script>
+import auth from 'firebase/auth'
 export default {
     name: "login",
+    methods: {
+        loginwithGoogle() {
+            firebase
+                .auth()
+                .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err));
+        },
+    },
 };
 </script>
