6cf600ac3ec74d384ddfe1ce89901c01d89513b3
Omar Tarek <omartarek711@gmail.com>
2021-06-08 23:02:36 +0200

    Create Auth Protection for Chat page

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,37 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import chat from '@/pages/chat'
 import login from '@/pages/login'
+import auth from "firebase/auth";
 
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'chat',
-      component: chat
+      component: chat,
+      beforeEnter(to, from, next) {
+        if (!firebase.auth().currentUser) {
+          next('/login');
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: login
+      component: login,
+      beforeEnter(to, from, next) {
+        if (firebase.auth().currentUser) {
+          next('/');
+        } else {
+          next();
+        }
+      }
     },
   ],
   mode: 'history',
-})
+});
