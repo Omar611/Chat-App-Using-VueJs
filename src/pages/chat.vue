de6bf40f53c6f1491a7999f1720c6f54254af622
Omar Tarek <omartarek711@gmail.com>
2021-06-09 18:04:51 +0200

    Create a sidebar component

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    <h2>Chat Page</h2>
-  </div>
+    <div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3 p-4 sidebar">
+                    <sidebar/>
+                </div>
+                <div class="col-md-9 content">Content</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import sidebar from "@/sidebar/sidebar";
+import Sidebar from '../sidebar/sidebar.vue';
+
 export default {
-  name: 'chat'
-}
+    name: "chat",
+    methods: {},
+    components: {
+      sidebar,
+    }
+};
 </script>
+
+<style scoped>
+.sidebar {
+    height: 100vh;
+    background-color: #1d1c1d;
+    overflow: auto;
+}
+</style>
---
+++ src/sidebar/sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h2 class="text-light text-center">#Slack with VueJs#</h2>
+        <p class="text-light text-center mb-5">
+            Realtime communication at it's best
+        </p>
+        <div class="text-center my-4">
+            <span>
+                <img
+                    :src="getCurrentUser.photoURL"
+                    alt="user avatar"
+                    class="rounded-circle mt2"
+                    height="100px"
+                />
+            </span>
+            <span>
+                <h3 class="text-light" v-text="getCurrentUser.displayName"></h3>
+            </span>
+        </div>
+        <div class="text-center">
+            <button @click="logout" class="btn btn-outline-light">
+                Logout
+            </button>
+        </div>
+        <hr class="bg-light m-4" />
+    </div>
+</template>
+
+<script>
+import auth from "firebase/auth";
+import { mapGetters } from "vuex";
+
+export default {
+    name: "sidebar",
+    computed: {
+        ...mapGetters(["getCurrentUser"]),
+    },
+    methods: {
+        logout() {
+            firebase.auth().signOut();
+            this.$store.dispatch("setUser", null);
+            setTimeout(() => {
+                this.$router.push("/login");
+            }, 100);
+        },
+    },
+};
+</script>
+
+<style scoped>
+</style>
