fad0a1692928be74c44085fab401e931b1cc100c
Omar Tarek <omartarek711@gmail.com>
2021-06-15 17:55:46 +0200

    Add notification functionality to private messages

--- src/sidebar/Users.vue
+++ src/sidebar/Users.vue
@@ -27,7 +27,14 @@
                             alt="avatar"
                         />
                     </span>
-                    <h5 class="ml-2">{{ user.name }}</h5>
+                    <h5 class="ml-2">
+                        {{ user.name }}
+                        <span
+                            class="badge badge-light float-right ml-3"
+                            v-if="getNotification(user) >= 1"
+                            >{{ getNotification(user) }}</span
+                        >
+                    </h5>
                     <span class="online" v-if="isonline(user)"></span>
                     <span class="offline" v-else></span>
                 </a>
@@ -39,6 +46,7 @@
 <script>
 import databse from "firebase/database";
 import { mapGetters } from "vuex";
+import mixin from "@/mixins";
 
 export default {
     name: "users",
@@ -48,10 +56,22 @@ export default {
             usersRef: firebase.database().ref("users"),
             connectedRef: firebase.database().ref(".info/connected"),
             presenceRef: firebase.database().ref("presence"),
+            PrivateMessagesRef: firebase.database().ref("privateMessages"),
+            notifCount: [],
+            channel: null,
+            channels: [],
         };
     },
+    mixins: [mixin],
     computed: {
-        ...mapGetters(["getCurrentUser", "getCurrentChannel"]),
+        ...mapGetters(["getCurrentUser", "getCurrentChannel", "isPrivate"]),
+    },
+    watch: {
+        isPrivate() {
+            if (!this.isPrivate) {
+                this.resetNotifications();
+            }
+        },
     },
     methods: {
         addLinstners() {
@@ -70,6 +90,18 @@ export default {
                 if (this.getCurrentUser.uid !== snapshot.key) {
                     //  Pass to user status method
                     this.addStatusToUser(snapshot.key);
+                    const channelId = this.getChannelID(snapshot.key);
+                    this.PrivateMessagesRef.child(channelId).on(
+                        "value",
+                        (snapshot) => {
+                            this.handelNotifications(
+                                channelId,
+                                this.getCurrentChannel.id,
+                                this.notifCount,
+                                snapshot
+                            );
+                        }
+                    );
                 }
             });
 
@@ -78,6 +110,9 @@ export default {
                 if (this.getCurrentUser.uid !== snapshot.key) {
                     //  Pass to user status method
                     this.addStatusToUser(snapshot.key, false);
+                    this.PrivateMessagesRef.child(
+                        this.getChannelID(snapshot.key)
+                    ).off();
                 }
             });
             // Return connected for each connected user
@@ -105,9 +140,29 @@ export default {
                 ? userId + "/" + this.getCurrentUser.uid
                 : this.getCurrentUser.uid + "/" + userId;
         },
+        resetNotifications(user = null) {
+            let channelId = null;
+            if (user !== null) {
+                channelId = this.getChannelID(user.uid);
+            } else {
+                channelId = this.channel.id;
+            }
+            const index = this.notifCount.findIndex((el) => el.id == channelId);
+            if (index !== -1) {
+                this.notifCount[index].total =
+                    this.notifCount[index].lastKnownTotal;
+                this.notifCount[index].notif = 0;
+            }
+        },
         changeChannel(user) {
+            if (this.channel == null) {
+                this.resetNotifications(user);
+            } else {
+                this.resetNotifications();
+            }
             const chanhelId = this.getChannelID(user.uid);
             const channel = { id: chanhelId, name: user.name };
+            this.channel = channel;
             this.$store.dispatch("setPrivate", true);
             this.$store.dispatch("setCurrentChannel", channel);
         },
@@ -115,10 +170,21 @@ export default {
             const channelId = this.getChannelID(user.uid);
             return this.getCurrentChannel.id === channelId;
         },
+        getNotification(user) {
+            const channelId = this.getChannelID(user.uid);
+            let notif = 0;
+            this.notifCount.forEach((el) => {
+                if (el.id == channelId) {
+                    notif = el.notif;
+                }
+            });
+            return notif;
+        },
         detachLinstners() {
             this.usersRef.off();
             this.presenceRef.off();
             this.connectedRef.off();
+            this.PrivateMessagesRef.off();
         },
     },
     mounted() {
